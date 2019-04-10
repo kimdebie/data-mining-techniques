4488a19212ad9c4c262f843f543697466a159566
kimdebie <kimdebie@outlook.com>
2019-04-10 10:12:28 +0200

    cleaning time

--- Assignment1/preprocess.py
+++ Assignment1/preprocess.py
@@ -6,6 +6,7 @@ import pandas as pd
 from datetime import datetime
 from scipy import stats
 import numpy as np
+from sklearn import preprocessing
 #import pyplot.matplotlib as plt
 
 def load(filename):
@@ -18,17 +19,28 @@ def load(filename):
     data["time"] = data["time"].apply(lambda x: datetime.strptime(x, "%Y-%m-%d %H:%M:%S.%f"))
 
     # save data on day level (change this line for different granularity)
-    data["time"] = data["time"].apply(lambda x: x.date())
+    data["time"] = data["time"].apply(lambda x: x.replace(hour=0,minute=0,second=0,microsecond=0).timestamp()/ (60*60))
 
-    # take mean values (aggregating at the specified datetime level)
-    data = data.groupby(["id", "time", "variable"]).mean().reset_index()
-
-    #print(data.head())
+    # remove outliers (technically cleaning but hard to do later!)
+    no_outliers = data.groupby(["id", "variable"])["value"].transform(lambda group: no_outlier(group)).to_frame()
+    data = data[no_outliers.any(axis=1)]
 
     # variables as columns
-    pivoted = data.pivot_table(index=['id', 'time'], columns='variable', values='value').reset_index()
+    data = data.pivot_table(index=['id', 'time'], columns='variable', values='value').reset_index()
+
+    # aggregating methods for each column (mean or sum) depending on data semantics
+    aggtypes = {'mood': 'mean', 'circumplex.arousal': 'mean', 'circumplex.valence': 'mean',
+                'appCat.builtin': 'sum', 'appCat.communication': 'sum', 'appCat.entertainment': 'mean',
+                'appCat.finance': 'sum', 'appCat.game': 'sum', 'appCat.office': 'sum', 'appCat.other': 'sum',
+                'appCat.social': 'sum', 'appCat.travel': 'sum', 'appCat.unknown': 'sum',
+                'appCat.utilities': 'sum', 'appCat.weather': 'sum', 'call': 'sum', 'screen': 'sum', 'sms': 'sum',
+                'activity': 'mean'}
+
+    # apply aggregations (at the specified datetime level)
+    pivoted = data.groupby(["id", "time"]).agg(aggtypes).reset_index()
 
     print(pivoted.shape)
+    print(pivoted.head())
 
     return pivoted
 
@@ -41,13 +53,21 @@ def clean(data):
     # note: the list may be extended with other values that must be present
     data = data.dropna(subset=['mood'])
 
-    # instead of exact datetime, calculate datetime relative to first datetime (in miliseconds)
+    # instead of exact datetime, calculate datetime relative to first datetime (timediff in hours)
     data['time'] = data.groupby('id')['time'].transform(lambda x: x - x.min())
 
-
     print(data.head())
-
     print(data.shape)
+
+    # https://scikit-learn.org/stable/modules/preprocessing.html
+    # super useful library for preprocessing data!
+
+    # https://chrisalbon.com/python/data_wrangling/pandas_normalize_column/
+    # uses that library - seems a good idea!
+
+    # some other blogs i came across:
+    # https://www.analyticsvidhya.com/blog/2016/07/practical-guide-data-preprocessing-python-scikit-learn/
+    # https://medium.com/@sidereal/feature-preprocessing-for-machine-learning-2f165d12012a
 
     data.to_csv('cleaned.csv')
 
@@ -66,3 +86,12 @@ def create_features_temporal(clean_data):
     raise NotImplementedError
 
     return featurized_data
+
+def no_outlier(x, z=3):
+
+    '''Detect outliers by calculating z-score. A value is an outlier if it is
+    more than z z-scores out'''
+
+    zscores = (x - x.mean()).div(x.std() + 1e-10)
+
+    return zscores.apply(lambda x: False if pd.notnull(x) and x > z else True)

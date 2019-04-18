4bd489d4ea312d1a9b548b925662c6e47fc65065
kimdebie <kimdebie@outlook.com>
2019-04-18 17:22:28 +0200

    benchmark

--- Assignment1/benchmark.py
+++ Assignment1/benchmark.py
@@ -5,44 +5,24 @@ import matplotlib.pyplot as plt
 def benchmark(data):
 
     '''Implements a benchmark metric for predicting mood: mood is the same as the previous day'''
-    print(len(data))
-    data = data[["id", "time", "mood"]]
+
+    data = data[["id", "time", "mood", "mood_lag1"]]
 
     data["mood"].hist()
     #plt.show()
 
-    # predicted mood is mood of previous day
-    data["predicted_mood"] = data["mood"].shift(1)
-
-    # we can only do this for observations with 1 day in between
-    data = data[data["time"].diff() == 24].dropna()
-
-    data["accuratehighbound"] = data["mood"] < data["predicted_mood"] * 1.05
-    data["accuratelowbound"] =  data["mood"] > data["predicted_mood"] * 0.95
+    data["accuratehighbound"] = data["mood"] < data["mood_lag1"] * 1.05
+    data["accuratelowbound"] =  data["mood"] > data["mood_lag1"] * 0.95
     data["accurate"] = data[["accuratelowbound", "accuratehighbound"]].all(axis='columns')
 
-    print(data.head())
+    acc = data["accurate"].sum() / data["accurate"].count()
 
-    accuracy = data["accurate"].sum() / data["accurate"].count()
-    print(accuracy)
+    accuracy = data["accurate"].tolist()
 
-    rsquared = r2_score(data["mood"], data["predicted_mood"])
-    mse = mean_squared_error(data["mood"], data["predicted_mood"])
+    rsquared = r2_score(data["mood"], data["mood_lag1"])
+    mse = mean_squared_error(data["mood"], data["mood_lag1"])
 
-    ypred = data["predicted_mood"]
-    print(len(ypred.values))
-    Y_test = data["mood"]
-
-    # Define accuracy with 10% error range
-    accuracy = []
-    for i in range(len(ypred)):
-    	if ypred.values[i] < Y_test.values[i]*1.05 and ypred.values[i] > Y_test.values[i]*0.95:
-    		accuracy.append(1)
-    	else:
-    		accuracy.append(0)
-    acc = float(sum(accuracy)) / float(len(ypred.values))
-
-    plt.scatter(data['mood'], data['predicted_mood'])
+    plt.scatter(data['mood'], data['mood_lag1'])
     #plt.show()
 
     return mse, acc, accuracy

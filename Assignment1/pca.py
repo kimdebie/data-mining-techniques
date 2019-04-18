2b59e2843f12c602db65fb5b09e7fccf9f4ada26
kimdebie <kimdebie@outlook.com>
2019-04-18 07:35:40 +0200

    push

--- Assignment1/pca.py
+++ Assignment1/pca.py
@@ -1,1 +1,22 @@
-import sklearn 
+import pandas as pd
+from pandas.plotting import autocorrelation_plot
+from pandas.plotting import lag_plot
+import matplotlib.pyplot as plt
+
+def autocorrelation(file):
+
+    df = pd.read_csv(file)
+
+    df = df[["id", "time", "mood"]]
+
+    for id in df["id"].unique():
+
+        series = df[df["id"] == id].mood
+        #autocorrelation_plot(series)
+        lag_plot(series)
+        plt.xlabel("Mood at current timepoint")
+        plt.ylabel("Mood at next timepoint")
+        plt.title("Lag plot for user " + id)
+        plt.show()
+
+autocorrelation('cleaned_normalized.csv')
